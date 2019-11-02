a607ac49fd3a2d9e58126be55bc4ce802f0ccb50
Benny <bcheng96@umd.edu>
2019-11-02 15:08:45 -0400

    added side navigation

--- app/Routes.js
+++ app/Routes.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import routes from './constants/routes';
 import App from './containers/App';
+import SideBar from './components/SideBar';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
 
 export default () => (
   <App>
-    <Switch>
-      <Route path={routes.COUNTER} component={CounterPage} />
-      <Route path={routes.HOME} component={HomePage} />
-    </Switch>
+    {/* <SideBar/> */}
+    <SideBar>
+      <Switch>
+        <Route path={routes.COUNTER} component={CounterPage} />
+        <Route path={routes.HOME} component={HomePage} />
+      </Switch>
+    </SideBar>
   </App>
 );
